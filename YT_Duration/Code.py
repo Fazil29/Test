8a0eeaf71de3d8d9e13f5e3c08d37fcc69060de4
DemonKillerh <40468574+Fazil29@users.noreply.github.com>
2020-05-27 22:14:18 +0530

    Update Code.py

--- YT_Duration/Code.py
+++ YT_Duration/Code.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
 import time
 import datetime
+from selenium.webdriver.common.keys import Keys
 
 Duration = 0
 Driver_Path = r"C:\Users\Fazil\Automation Drivers\chromedriver.exe"
-Playlist_URL = r'https://www.youtube.com/playlist?list=PLF_7kfnwLFCF_VxKKAhHSLryCsJr3GW71'
+Playlist_URL = r'https://www.youtube.com/playlist?list=PL-osiE80TeTvviVL0pJGX5mZCo7CAvIuf'
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 WebDriver = webdriver.Chrome(Driver_Path, options = options)
-
+WebDriver.maximize_window()
 WebDriver.get(Playlist_URL)
-time.sleep(2)
+time.sleep(1)
+WebDriver.find_element_by_tag_name('html').send_keys(Keys.END)
+time.sleep(3)
 Container = WebDriver.find_element_by_xpath(r'//*[@id="contents"]')
 
 def hms_to_s(s):
@@ -21,6 +24,7 @@ def hms_to_s(s):
     return t
 
 for word in Container.text.split():
-    if len(word)>1 and ':' in word:
+    if len(word)>4 and ':' in word:
+        print(word)
         Duration = Duration + hms_to_s(word)/60
 print(int(Duration),'minutes')
